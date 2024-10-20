e4be7a71d7a27c00d69010b4d6897d2b3afd6d02
harsh <hkrai1372@gmail.com>
2024-10-20 20:02:04 +0530

    react

--- React/cohort/moz-todo-react/src/App.jsx
+++ React/cohort/moz-todo-react/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from "react";
 import { useEffect } from "react";
 function App(){
-  return <div>
-  
-    <Counter/>
+    return(
+      <div>
+        hi there 2
+        </div>
+    )
+}
+const style={width :200 , backgroundColor:"white",borderRadius :10,borderColor:"grey"};
+function postcomponent(){
+  return <div style={style}>
+    <img src={""} alt="" />
   </div>
 }
-function Counter(){
-  const [count, setCount] = useState(0);
-  function increasecount(){
-    setCount(count + 1);
-  }
-
-  function decreasecount(){
-    setCount(count - 1);
+export default App;
+//   return <div>
   
-  }
-  useEffect(() => {
-    setInterval(function(){
-      setCount(function(x){
-        return x +1;
-      })
-  },1000);
-  }, [])
+//     <Counter/>
+//   </div>
+// }
+// function Counter(){
+//   const [count, setCount] = useState(0);
+//   function increasecount(){
+//     setCount(count + 1);
+//   }
+
+//   function decreasecount(){
+//     setCount(count - 1);
+  
+//   }
+//   useEffect(() => {
+//     setInterval(function(){
+//       setCount(function(x){
+//         return x +1;
+//       })
+//   },1000);
+//   }, [])
   
 
-  return <div>
-    <h1 id="text">Count: {count}</h1>
-    <button onClick={increasecount}>increasecount</button>
-    <button onClick={decreasecount}>decreasecount</button>
-  </div>
-}
-export default App
+//   return <div>
+//     <h1 id="text">Count: {count}</h1>
+//     <button onClick={increasecount}>increasecount</button>
+//     <button onClick={decreasecount}>decreasecount</button>
+//   </div>
+//}
